451c050962bbde8e0462e9bb70edf56a30d4700c
Owen <315872858@qq.com>
2022-04-22 18:30:39 +0800

    Update background.js

--- src/background.js
+++ src/background.js
@@ -74,9 +74,9 @@ if (!isDevelopment) {
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    // if (process.platform !== 'darwin') {
+    //     app.quit()
+    // }
 })
 
 app.on('activate', () => {
