8bf1ad99a3ab12d1db30a049277c55a2b578c1a9
Owen <zeelyn@qq.com>
2023-07-11 11:17:25 +0800

    1.1.2

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -20,6 +20,7 @@
                     <el-option label="官方：https://translate.google.com" value="https://translate.google.com" />
                     <el-option label="镜像：https://translate.amz.wang" value="https://translate.amz.wang" />
                     <el-option label="镜像：https://translate.3822808.com" value="https://translate.3822808.com" />
+                    <el-option label="镜像：https://gt.cky.codes" value="https://gt.cky.codes" />
                 </el-select>
             </el-form-item>
         </el-form>
